9361b9890de1d72f3e124378af39158a435629d6
Zeydel <mads_seydel@yahoo.dk>
2018-06-05 11:17:48 +0200

    Raw Material Table

--- 23_Final/WebContent/html/rawMaterial.js
+++ 23_Final/WebContent/html/rawMaterial.js
@@ -1,3 +1,23 @@
-/**
- * 
- */
+$(document).ready(function(){
+	loadRawMaterials()
+
+})
+
+function loadRawMaterials(){
+	$.ajax({
+		url : '/23_Final/rest/rawmaterial',
+		type : 'GET',
+		dataType : 'json',
+		success : function(data){
+			$.each(data, function(i, el){
+				$('#rawMaterialTableBody').append(generateRawMaterialHTML(el));
+			})
+		}
+	})
+}
+
+function generateRawMaterialHTML(RawMaterialDTO){
+	return '<tr><td>' + RawMaterialDTO.rawMaterialID + '</td>' +
+	'<td>' + RawMaterialDTO.rawMaterialName + '</td>' +
+	'<td>' + RawMaterialDTO.supplier + '</td><td>'
+}
---
+++ 23_Final/src/rest/RawMaterial.java
@@ -0,0 +1,33 @@
+package rest;
+
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import dao.NpStorage;
+import dto.RawMaterialDTO;
+import dto.UserDTO;
+
+@Path("rawmaterial")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class RawMaterial {
+	static NpStorage Storage = new NpStorage();
+	
+	@POST
+	public boolean createUser(UserDTO user) {
+		Storage.getUser().createUser(user);
+		return true;
+	}
+	
+	@GET
+	public List<RawMaterialDTO> getUsers() {
+		return Storage.getRawMaterial().getRawMaterialList();
+	}
+
+}
