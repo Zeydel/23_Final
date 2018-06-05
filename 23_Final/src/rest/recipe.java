c916979af5971fb0311c3221808f5274d7e42ca6
BenDamore <BenDamore@ben.clients.wireless.dtu.dk>
2018-06-05 11:27:41 +0200

    recipe.java fix fix

--- 23_Final/src/rest/recipe.java
+++ 23_Final/src/rest/recipe.java
@@ -1,11 +1,6 @@
 package rest;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -14,8 +9,10 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import dao.NpStorage;
-import dao.NpUserDAO;
-import dto.UserDTO;
+import dto.RecipeDTO;
+
+
+
 
 @Path("recipes")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -24,14 +21,14 @@ public class recipe {
 	static NpStorage Storage = new NpStorage();
 	
 	@POST
-	public boolean createUser(UserDTO recipe) {
-		Storage.getUser().createUser(recipe);
+	public boolean createRecipe(RecipeDTO recipe) {
+		Storage.getRecipe().createRecipe(recipe);
 		return true;
 	}
 	
 	@GET
-	public List<UserDTO> getUsers() {
-		return Storage.getUser().getUserList();
+	public List<RecipeDTO> getRecipes() {
+		return Storage.getRecipe().getRecipeList();
 	}
 	
 }
