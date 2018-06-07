5731cc4b4e5b58856044751e51b9b8351245c71a
Isaac Irani <isaac@eywasystems.dk>
2018-06-07 14:09:39 +0200

    edit of names

--- 23_Final/src/rest/Login.java
+++ 23_Final/src/rest/Login.java
@@ -1,30 +1,29 @@
-//package rest;
-//
-//import java.util.List;
-//
-//import javax.ws.rs.Consumes;
-//import javax.ws.rs.GET;
-//import javax.ws.rs.POST;
-//import javax.ws.rs.Path;
-//import javax.ws.rs.Produces;
-//import javax.ws.rs.core.MediaType;
-//
-//import dto.UserDTO;
-//
-//@Path("login")
-//@Consumes(MediaType.APPLICATION_JSON)
-//@Produces(MediaType.APPLICATION_JSON)
-//public class Login {
-//
-//	@POST
-//	public boolean createUser(UserDTO user) {
-//		Storage.getUser().createUser(user);
-//		return true;
-//	}
-//	
-//	@GET
-//	public List<UserDTO> getUsers() {
-//		return Storage.getUser().getUserList();
-//	}
-//	
-//}
+package rest;
+
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import dto.UserDTO;
+
+@Path("login")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class Login {
+
+	@POST
+	public boolean verifyLogin(UserDTO user) {
+		
+	}
+	
+	@GET
+	public List<UserDTO> getUsers() {
+		return Storage.getUser().getUserList();
+	}
+	
+}
