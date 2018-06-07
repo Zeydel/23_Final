2eb47967d7852df21081c7dbd2c898248c09b15b
fredehus <fredehus@gmail.com>
2018-06-07 12:41:13 +0200

    LORT

---
+++ 23_Final/src/dbdao/DbRawMaterialDAO.java
@@ -0,0 +1,59 @@
+package dbdao;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import dbdao.Connector;
+import dto.DALException;
+import dto.RawMaterialDTO;
+
+public class DbRawMaterialDAO {
+
+	public RawMaterialDTO getRawMaterial(int rawMaterialID) throws DALException {
+		ResultSet rs = Connector.doQuery("SELECT * FROM rawMaterial WHERE rawMaterialID = " + rawMaterialID);
+		try {
+			if(!rs.first()) throw new DALException("The rawmaterial with id " + rawMaterialID + " doesn't exist");
+			return new RawMaterialDTO (rs.getInt("rawMaterialID"), rs.getString("rawMaterialName"), rs.getString("supplier"));
+		}
+		catch (SQLException e) {throw new DALException(null, e);
+		}
+	}
+	
+	public List<RawMaterialDTO> getRawMaterialList() throws DALException{
+		List<RawMaterialDTO> list = new ArrayList<RawMaterialDTO>();
+		ResultSet rs = Connector.doQuery("SELECT * FROM rawMaterial");
+		try {
+			while(rs.next()) {
+				list.add(new RawMaterialDTO (rs.getInt("rawMaterialID"), rs.getString("rawMaterialName"), rs.getString("supplier")));
+			}
+		} catch (SQLException e) { throw new DALException("No list found", e); }
+		return list;
+	}
+	
+	public void createRawMaterial(RawMaterialDTO rawMaterial) throws DALException {
+		try {
+			Connector.doUpdate("INSERT INTO rawMaterial(rawMaterialID, rawMaterialName, supplier) VALUES"
+					+ "(" + rawMaterial.getRawMaterialID() + ", '" + rawMaterial.getRawMaterialName() + ", '" + rawMaterial.getSupplier() +"')");
+		} catch(DALException e) {throw new DALException("Rawmaterial not created");}
+	}
+	
+	
+	public void updateRawMaterial(RawMaterialDTO rawMaterial) throws DALException{
+		try{
+			Connector.doUpdate("UPDATE rawMaterial SET rawMaterialName = '"
+		+ rawMaterial.getRawMaterialName() + "', supplier = '" + rawMaterial.getSupplier() + "' WHERE rawMaterialID = " + rawMaterial.getRawMaterialID());
+		}
+		catch (DALException e) {throw new DALException("Rawmaterial with id " + rawMaterial.getRawMaterialID() + " doesn't exist.");}
+	}
+	
+	public void deleteRawMaterial(int rawMaterialID) throws DALException{
+		try{
+			Connector.doUpdate(
+					"DELETE FROM rawMaterial "
+					+ "WHERE rawMaterialID = " + rawMaterialID);
+			
+		}catch (DALException e) {throw new DALException("Rawmaterial with ID " + rawMaterialID + " doesn't exist");
+	}
+}
+}
+
---
+++ 23_Final/src/dbdao/DbRecipeComponentDAO.java
@@ -0,0 +1,84 @@
+package dbdao;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import dbdao.Connector;
+import dto.DALException;
+import dto.RecipeComponentDTO;
+
+
+public class DbRecipeComponentDAO {
+
+	public RecipeComponentDTO getRecipeComponent(int recipeID, int rawMaterialID) throws DALException {
+		ResultSet rs = Connector.doQuery("SELECT * FROM recipeComponent WHERE recipeID = " + recipeID + " AND rawMaterialID = " + rawMaterialID);
+	    try {
+	    	if (!rs.first()) throw new DALException("Receptkomponenten " + recipeID + " findes ikke");
+	    	return new RecipeComponentDTO(rs.getInt("recipeID"), rs.getInt("rawMaterialID"), rs.getFloat("nom_netto"), rs.getFloat("tolerance"));
+	    }
+	    catch (SQLException e) {throw new DALException(null, e); }
+	}
+
+	public List<RecipeComponentDTO> getRecipeComponentList(int recipeID) throws DALException {
+		List<RecipeComponentDTO> list = new ArrayList<RecipeComponentDTO>();
+		ResultSet rs = Connector.doQuery("SELECT * FROM receptkomponent WHERE recipeID = " + recipeID);
+		try
+		{
+			while (rs.next()) 
+			{
+				list.add(new RecipeComponentDTO(rs.getInt("recipeID"), rs.getInt("rawMaterialID"), rs.getFloat("nom_netto"), rs.getFloat("tolerance")));
+			}
+		}
+		catch (SQLException e) { throw new DALException("Recipe component with ID" + recipeID + " doesn't exist"); }
+		return list;
+	}
+
+
+	public List<RecipeComponentDTO> getRecipeComponentList() throws DALException {
+		List<RecipeComponentDTO> list = new ArrayList<RecipeComponentDTO>();
+		ResultSet rs = Connector.doQuery("SELECT * FROM recipeComponent");
+		try
+		{
+			while (rs.next()) 
+			{
+				list.add(new RecipeComponentDTO(rs.getInt("recipeID"), rs.getInt("rawMaterialID"), rs.getFloat("nom_netto"), rs.getFloat("tolerance")));
+			}
+		}
+		catch (SQLException e) { throw new DALException(null, e); }
+		return list;
+	}
+
+
+	public void createReceptKomp(RecipeComponentDTO recipeComponent) throws DALException {
+		try {
+		Connector.doUpdate(
+				"INSERT INTO recipeComponent(recipeID, rawMaterialID, nom_netto, tolerance) VALUES " +
+				"(" + recipeComponent.getRecipeID() + ", '" + recipeComponent.getRawMaterialID()+ "', '" + recipeComponent.getNom_netto() + "', '" + 
+				recipeComponent.getTolerance()+ "')"
+			);
+		} catch (DALException e) {throw new DALException("Recipe not created");}
+
+	}
+
+
+	public void updateRecipeComponent(RecipeComponentDTO recipeComponent) throws DALException {
+		try{
+		Connector.doUpdate(
+				"UPDATE recipeComponent SET nom_netto =  '" + recipeComponent.getNom_netto() + 
+				"', tolerance = '" + recipeComponent.getTolerance() + "' WHERE recipeID = " +
+				recipeComponent.getRecipeID()+ " AND rawMaterialID = " + recipeComponent.getRawMaterialID()
+		);
+		}catch(DALException e) {throw new DALException ("Recipe component with ID " + recipeComponent.getRecipeID() + " doesn't exist.");}
+
+	}
+	
+	public void deleteRecipeComponent(int recipeID, int rawMaterialID) throws DALException {
+		try {
+			Connector.doUpdate(
+					"DELETE FROM recipeComponent "
+					+ "WHERE recipeID = " + recipeID + " AND rawMaterialID = " + rawMaterialID ); 
+		
+		} catch (DALException e) {throw new DALException("Recipe component with id " + recipeID + " and rawMaterial ID " + rawMaterialID + " doesn't exist.");}
+	}
+
+}
