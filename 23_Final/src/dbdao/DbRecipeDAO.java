3487f6ea25dc38b0dd5758bdbce1c2a24a04e308
Zeydel <mads_seydel@yahoo.dk>
2018-06-07 10:53:19 +0200

    opdateret gitignore

--- 23_Final/src/dbdao/DbRecipeDAO.java
+++ 23_Final/src/dbdao/DbRecipeDAO.java
@@ -5,7 +5,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import connector01917.Connector;
 import daointerfaces01917.DALException;
 import dto01917.ReceptDTO;
 
