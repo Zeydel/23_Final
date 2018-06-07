17ba2b7013e0d0dca078d99dceb1dff24db7a2d9
Isaac Irani <isaac@eywasystems.dk>
2018-06-07 11:28:27 +0200

    header.js added

---
+++ 23_Final/WebContent/js/header.js
@@ -0,0 +1,4 @@
+
+$(document).ready(function(){
+	$( "<button class='back-button' onclick='location.href=\"menu.html\"' type='button'>Back to menu</button>" ).insertBefore( window.document.body.firstChild );
+});
