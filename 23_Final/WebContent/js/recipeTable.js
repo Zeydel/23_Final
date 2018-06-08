f9d3f1046c77261aba4cca68fd993d84738c385e
Zeydel <mads_seydel@yahoo.dk>
2018-06-08 14:04:16 +0200

    recipetable

--- 23_Final/WebContent/js/recipeTable.js
+++ 23_Final/WebContent/js/recipeTable.js
@@ -19,7 +19,21 @@ $(document).ready(function(){
 	function generateRecipeHTML(RecipeDTO){
 		return '<tr><td>' + RecipeDTO.recipeID + '</td>' +
 				'<td>' + RecipeDTO.recipeName + '</td>' +
-				'<td>' + '<button class="options" id="edituserbutton" onclick="location.href="> <i class="fas fa-edit"></i> </button>' +
-				'<button class="options" onclick="deleteUser($(this).val())" value=""> <i class="fas fa-trash-alt"></i> </button>' + '</td>' + '</tr>'
+				'<td>' + '<button class="options" id="editRecipeButton" onClick="location.href=\'editRecipeForm.html?id=' + RecipeDTO.recipeID +'\'' + '"> <i class="fas fa-edit"></i> </button>' +
+				'<button class="options" onclick="deleteRecipe($(this).val())" value="' + RecipeDTO.recipeID + '"> <i class="fas fa-trash-alt"></i> </button>' + '</td>' + '</tr>'
 
+	}
+	
+	function deleteRecipe(value){
+		event.preventDefault();
+		$.ajax({
+			url : '/23_Final/rest/recipes/' + value,
+			type : 'DELETE',
+			dataype : 'json',
+			contentType : ("application/json"),
+			success : function(){
+				$('#recipeTableBody').empty();
+				loadRecipes();
+			}
+		})
 	}
--- 23_Final/src/rest/Recipe.java
+++ 23_Final/src/rest/Recipe.java
@@ -2,15 +2,21 @@ package rest;
 
 import java.util.List;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import dbdao.DbStorage;
+import dto.CprDTO;
 import dto.DALException;
 import dto.RecipeDTO;
+import dto.RoleDTO;
+import dto.UserDTO;
+import dto.UserViewDTO;
 import npdao.NpStorage;
 
 
@@ -43,6 +49,43 @@ public class Recipe {
 			e.printStackTrace();
 		}
 		return null;
+	}
+	
+	@DELETE
+	@Path("{recipeID}")
+	public void deleteUser(@PathParam("recipeID")String recipeID) {
+		try {
+			Storage.getRecipe().deleteRecipe(Integer.parseInt(recipeID));
+		} catch (DALException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@GET
+	@Path("{recipeID}")
+	public RecipeDTO getRecipe(@PathParam("recipeID")String recipeID){
+		try {
+			return Storage.getRecipe().getRecipe(Integer.parseInt(recipeID));
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (DALException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	@POST
+	@Path("/edit")
+	public void editUser(RecipeDTO recipe) {
+		try {
+			Storage.getRecipe().updateRecipe(recipe);
+		} catch (DALException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 }
