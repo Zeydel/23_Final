dbbc1d4b76de6c4b93e3d4f506b12e59b66eacbf
Zeydel <mads_seydel@yahoo.dk>
2018-06-11 13:27:37 +0200

    Waterboy

--- 23_Final/src/ase/WeightTest.java
+++ 23_Final/src/ase/WeightTest.java
@@ -4,11 +4,16 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class WeightTest {
-	
-	
+
+
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		WeightFunction weight = new WeightFunction();
+		weight.clearInputStream();
+		weight.waitForInput();
+		System.out.println(weight.getWeight());
+
+		weight.writeInDisplay("12345678901234567890", 10);
 		System.out.println(weight.getWeight());
 		try {
 			weight.WeightTare();
@@ -19,7 +24,7 @@ public class WeightTest {
 		System.out.println(weight.getWeight());
 		weight.writeInDisplay("test", 1);
 		weight.returnToWeight();
-		weight.writeLongStringInDisplay("LAAAAAAAANG TEXT", 1);
+		//weight.writeLongStringInDisplay("LAAAAAAAANG TEXT", 1);
 		System.out.println("hej");
 		try {
 			System.out.println(weight.getStringFromDisplay("Tryk på noget"));
@@ -28,5 +33,4 @@ public class WeightTest {
 			e.printStackTrace();
 		}
 	}
-
 }
