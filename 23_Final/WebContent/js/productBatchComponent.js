6f99cab22382e514f8ad3bb88a2309c5ce610393
fredehus <fredehus@gmail.com>
2018-06-12 12:00:49 +0200

    productBatchComponent Virker!

--- 23_Final/WebContent/js/productBatchComponent.js
+++ 23_Final/WebContent/js/productBatchComponent.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
 			dataType : 'json',
 			success : function(data){
 				$.each(data, function(i, el){
-					$('#TableBody').append(generateProductBatchComponentHTML(el));
+					$('#PBCTableBody').append(generateProductBatchComponentHTML(el));
 				})
 			}
 		})
@@ -18,10 +18,9 @@ $(document).ready(function(){
 	
 	function generateProductBatchComponentHTML(ProductBatchComponentDTO){
 		return '<tr><td>' + ProductBatchComponentDTO.productbatchID + '</td>' +
-				'<td>' + ProductBatchComponentDTO.rawMaterialID + '</td>'+
+				'<td>' + ProductBatchComponentDTO.rawMaterialBatchID + '</td>'+
 				'<td>' + ProductBatchComponentDTO.tara + '</td>'+
 				'<td>' + ProductBatchComponentDTO.netto + '</td>'+
-				'<td>' + ProductBatchComponentDTO.userID + '</td>' + '<td>' + '<button class="options" id="edituserbutton"> <i class="fas fa-edit"></i> </button>' +
-				'<button class="options" onclick="deleteUser($(this).val())" value=""> <i class="fas fa-trash-alt"></i> </button>' + '</td>' + '</tr>';
+				'<td>' + ProductBatchComponentDTO.userID + '</td>' + '</tr>';
 
 	}
